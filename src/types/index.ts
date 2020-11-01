076bc8e1a406b8840e2b8e8bc8658121ad0f22d6
wang.haoyu <wanghaoyu@wanghaoyudeMacBook-Pro.local>
2020-11-01 21:04:18 +0800

    feat(简单请求): simple axios
    简单请求，example目录完善

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'post'
+  | 'POST'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
