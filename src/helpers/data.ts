6bc2468603ca6a2c39ec7fa43c60ec7fedcbfbe4
wang.haoyu <wanghaoyu@wanghaoyudeMBP.lan>
2020-11-03 20:22:04 +0800

    feat(post data format): formatdata
    处理post请求data格式化实现

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -4,5 +4,12 @@
     A Document, in which case it is serialized before being sent.
     An XMLHttpRequestBodyInit, which per the Fetch spec can be a Blob, BufferSource, FormData, URLSearchParams, or USVString object.
     null
-    平常我们传递的JSON对象并不能被send，所以通常我们是使用JSON.string()转为字符串，此时他就是USVString类型了。可以调用xhr.send方法进行http传输了。
+    平常我们传递的JSON对象并不能被send，所以通常我们是使用JSON.stringify()转为字符串，此时他就是USVString类型了。可以调用xhr.send方法进行http传输了。
 */
+import { isPlaneObject } from './utlis'
+export function transformRequest(data: any): any {
+  if (isPlaneObject(data)) {
+    return JSON.stringify(data)
+  }
+  return data
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import { xhr } from './xhr'
 import { buildUrl } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig) {
   processCofing(config)
@@ -9,11 +10,16 @@ function axios(config: AxiosRequestConfig) {
 
 function processCofing(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildUrl(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
