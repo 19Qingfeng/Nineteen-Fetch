35e00ee2567d2304a9234e631e23a8ced3b9ce04
wang.haoyu <wanghaoyu@wanghaoyudeMacBook-Pro.local>
2020-11-21 15:00:41 +0800

    test(Cancel Demo):

--- examples/cancel/app.ts
+++ examples/cancel/app.ts
@@ -2,39 +2,36 @@ import axios, { Canceler } from '../../src/index'
 
 const CancelToken = axios.CancelToken
 const source = CancelToken.source()
-axios
-  .get('/cancel/get', {
-    cancelToken: source.token
-  })
-  .catch(function(e) {
-    if (axios.isCancel(e)) {
-      console.log('Request canceled', e.message, '2')
-    }
-  })
+
+axios.get('/cancel/get', {
+  cancelToken: source.token
+}).catch(function(e) {
+  if (axios.isCancel(e)) {
+    console.log('Request canceled', e.message)
+  }
+})
 
 setTimeout(() => {
   source.cancel('Operation canceled by the user.')
 
   axios.post('/cancel/post', { a: 1 }, { cancelToken: source.token }).catch(function(e) {
     if (axios.isCancel(e)) {
-      console.log(e.message,'1')
+      console.log(e.message)
     }
   })
 }, 100)
 
 let cancel: Canceler
 
-axios
-  .get('/cancel/get', {
-    cancelToken: new CancelToken(c => {
-      cancel = c
-    })
+axios.get('/cancel/get', {
+  cancelToken: new CancelToken(c => {
+    cancel = c
   })
-  .catch(function(e) {
-    if (axios.isCancel(e)) {
-      console.log('Request canceled','3')
-    }
-  })
+}).catch(function(e) {
+  if (axios.isCancel(e)) {
+    console.log('Request canceled')
+  }
+})
 
 setTimeout(() => {
   cancel()
